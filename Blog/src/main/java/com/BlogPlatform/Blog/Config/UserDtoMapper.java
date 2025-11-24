cbc7ab21a1dee67c2da240b59da0e3ec9db24614
Gurwinder-dotcom <gurwinderofficial9@gmail.com>
2025-11-24 15:45:53 +0530

    f_1: add modelmapper

--- Blog/src/main/java/com/BlogPlatform/Blog/Config/UserDtoMapper.java
+++ Blog/src/main/java/com/BlogPlatform/Blog/Config/UserDtoMapper.java
@@ -3,29 +3,33 @@ package com.BlogPlatform.Blog.Config;
 
 import com.BlogPlatform.Blog.Entity.User;
 import com.BlogPlatform.Blog.Payloads.UserDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDtoMapper {
 
+    @Autowired
+    private ModelMapper modelMapper;
 
-    public User fromDto(UserDto userDto){
-        User user = new User();
-        user.setId(userDto.getId());
-        user.setName(userDto.getName());
-        user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
-        user.setAbout(userDto.getAbout());
-        return user;
+    public User fromDto(UserDto userDto) {
+//        User user = new User();
+//        user.setId(userDto.getId());
+//        user.setName(userDto.getName());
+//        user.setEmail(userDto.getEmail());
+//        user.setPassword(userDto.getPassword());
+//        user.setAbout(userDto.getAbout());
+        return modelMapper.map(userDto,User.class);
     }
 
-    public UserDto toDto(User user){
-        UserDto userDto =new UserDto();
-        userDto.setId(user.getId());
-        userDto.setName(user.getName());
-        userDto.setEmail(user.getEmail());
-        userDto.setPassword(user.getPassword());
-        userDto.setAbout(user.getAbout());
-        return userDto;
+    public UserDto toDto(User user) {
+//        UserDto userDto = new UserDto();
+//        userDto.setId(user.getId());
+//        userDto.setName(user.getName());
+//        userDto.setEmail(user.getEmail());
+//        userDto.setPassword(user.getPassword());
+//        userDto.setAbout(user.getAbout());
+        return modelMapper.map(user, UserDto.class);
     }
 }
